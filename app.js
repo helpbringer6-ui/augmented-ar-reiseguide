0efe7b78011cbed6004b4f9ddfd7b855b20e825d
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-03 11:29:03 -0500

    Update log message to be more descriptive.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Augmented AR Reiseguide geladen.');
+    console.log('Augmented AR Reiseguide successfully loaded. Ready to implement AR functionalities.');
     // Hier können die AR-Funktionen implementiert werden.
 });

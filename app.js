d01dc15a9dc63a284df7b20e14dc7b45fad6f223
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-03 11:29:31 -0500

    Refactor console log message for clarity.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Augmented AR Reiseguide successfully loaded. Ready to implement AR functionalities.');
+    console.log('Augmented AR Reiseguide loaded successfully. Now ready to implement AR functionalities.');
     // Hier können die AR-Funktionen implementiert werden.
 });
